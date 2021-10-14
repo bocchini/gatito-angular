98aaa131d79ee68b32ced9618c599f83f9d7c8ea
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-10-14 09:13:33 -0300

    Listagem de animais e correção do novo usuário

---
+++ src/app/componentes/cartao/cartao.module.ts
@@ -0,0 +1,10 @@
+import { CartaoComponent } from './cartao.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [CartaoComponent],
+})
+export class CartaoModule {}
