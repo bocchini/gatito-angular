424fc673e55edd63a59757d6f3aa747907c01701
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-10-14 09:09:10 -0300

    criação dos vallidator e inicio da listagem de informações dos animais

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
   url = 'http://localhost:3000/user/login';
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post(this.url, {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        this.url,
+        {
+          userName: usuario,
+          password: senha,
+        },
+        {
+          observe: 'response',
+        }
+      )
+      .pipe(
+        tap((response) => {
+          const authToken = response.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -6,10 +6,14 @@ import { NovoUsuario } from './novo-usuario';
   providedIn: 'root',
 })
 export class NovoUsuarioService {
-  urlApi = 'http://localhost:3000/user/signup';
+  urlApi = 'http://localhost:3000/';
   constructor(private httpCliente: HttpClient) {}
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.httpCliente.post(this.urlApi, novoUsuario);
+    return this.httpCliente.post(`${this.urlApi}user/signup`, novoUsuario);
+  }
+
+  verificaUsuarioExistente(nomeDoUsuario: string) {
+    return this.httpCliente.get(`${this.urlApi}user/exists/${nomeDoUsuario}`);
   }
 }
